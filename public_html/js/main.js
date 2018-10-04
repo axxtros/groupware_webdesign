c7b3be30f3a305ba365807e385f287d49892f5ef
axtros <axtros@gmail.com>
2018-10-04 13:32:11 +0200

    v0.6

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -3,11 +3,26 @@
  */
 
 function init() {
-  sideMenuBgHeightInit();
+  //sideMenuBgHeightInit();
 }
 
 function sideMenuBgHeightInit() {  
   var sideMenuEmyptyDivTop = $('.side-menu-empty-sc').position().top;
   var footerContainerDivTop = $('.footer-side-sc').position().top;
   $('.side-menu-empty-sc').css('height', footerContainerDivTop - sideMenuEmyptyDivTop + 'px');  
+}
+
+function blockPanelCtrl(blockPanelAnchor) {  
+  var closeIconDiv = $(blockPanelAnchor).children().children().next();
+  var openIconDiv = $(blockPanelAnchor).children().children().next().next();
+  var contentPanel = $(blockPanelAnchor).next();  
+  $(contentPanel).toggle("fast", function() {
+    if($(openIconDiv).css('display') === 'none') {
+      $(closeIconDiv).css('display', 'none');
+      $(openIconDiv).css('display', 'block');
+    } else {
+      $(closeIconDiv).css('display', 'block');
+      $(openIconDiv).css('display', 'none');
+    }
+  });
 }
